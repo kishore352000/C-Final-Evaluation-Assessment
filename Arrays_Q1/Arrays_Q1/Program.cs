74c228f1adfde26c78c43744fe20fdeee2556747
kishore <Kishore.Lakkireddy@ivycomptech.com>
2022-10-07 11:40:28 +0530

    importing files

---
+++ Arrays_Q1/Arrays_Q1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Arrays_Q1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[100];
+            int n, min, max, i;
+            Console.WriteLine("Enter number of elements in the array=");
+            n = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter " + n+ " "+ "elements in the array");
+            for (i = 0; i < n; i++)
+            {
+                Console.Write(" element"+" " +i+" " +"=");
+                arr[i] = int.Parse(Console.ReadLine());
+
+            }
+            min = arr[0];
+            max = arr[0];
+            for (i = 1; i < n; i++)
+            {
+                if ( arr[i]<min)
+                {
+                    min = arr[i];
+                }
+                if (arr[i] > max)
+                {
+                    max = arr[i];
+                }
+            }
+            Console.WriteLine("The maximum element in the array is" + max);
+            Console.WriteLine("The minimum element in the array is" + min);
+
+        }
+    }
+}
